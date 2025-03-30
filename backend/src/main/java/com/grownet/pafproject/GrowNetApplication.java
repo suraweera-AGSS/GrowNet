ce2a252feb6803745e476a1ecc0f1d0f85384319
Sithum Suraweera <sithumsuraweera01@gmail.com>
2025-03-30 17:44:13 +0530

    chore: set up backend & frontend folder structure using Springboot Initializer & Vite

---
+++ backend/src/main/java/com/grownet/pafproject/GrowNetApplication.java
@@ -0,0 +1,13 @@
+package com.grownet.pafproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowNetApplication.class, args);
+	}
+
+}
